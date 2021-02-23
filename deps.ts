20b22d7299dd6bb74ab2c3592f34b10e13d07f74
n-ari <fukunari.r.aa@m.titech.ac.jp>
2021-02-24 00:43:44 +0900

    without std/node

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export * as Bson from "./bson/mod.ts";
 export { createHash } from "https://deno.land/std@0.83.0/hash/mod.ts";
-export { pbkdf2Sync } from "https://deno.land/std@0.83.0/node/_crypto/pbkdf2.ts";
+export { pbkdf2Sync } from "https://raw.githubusercontent.com/n-ari/deno_pbkdf2/master/mod.ts";
 export { HmacSha1 } from "https://deno.land/std@0.83.0/hash/sha1.ts";
 export { HmacSha256 } from "https://deno.land/std@0.83.0/hash/sha256.ts";
 export * from "https://deno.land/x/bytes_formater/mod.ts";
